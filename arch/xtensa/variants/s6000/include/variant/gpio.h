839f10d4ba5abad47f6962f752ebfc1b82e13706
Piotr Kubaj <pkubaj@anongoth.pl>
2018-09-11 13:55:38 +0200

    Clear whole branch in preparation for dev branch.
    Signed-off-by: Piotr Kubaj <pkubaj@anongoth.pl>

--- arch/xtensa/variants/s6000/include/variant/gpio.h
+++
@@ -1,6 +0,0 @@
-#ifndef _XTENSA_VARIANT_S6000_GPIO_H
-#define _XTENSA_VARIANT_S6000_GPIO_H
-
-extern int s6_gpio_init(void);
-
-#endif /* _XTENSA_VARIANT_S6000_GPIO_H */
--- arch/xtensa/variants/s6000/include/variant/irq.h
+++
@@ -1,9 +0,0 @@
-#ifndef __XTENSA_S6000_IRQ_H
-#define __XTENSA_S6000_IRQ_H
-
-#define NO_IRQ		(-1)
-
-extern void variant_irq_enable(unsigned int irq);
-extern void variant_irq_disable(unsigned int irq);
-
-#endif /* __XTENSA_S6000_IRQ_H */
